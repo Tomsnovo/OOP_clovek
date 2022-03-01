93a3c5f5de4f991e2ffcabf7b2a5aa6e61a32a1a
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2022-03-01 09:13:55 +0100

    Přidejte soubory projektu.

---
+++ OOP_clovek/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_clovek
+{
+    public class Clovek
+    {
+        public string name { get; }
+        public string surname { get; }
+        public DateTime birth { get; }
+        int stamina { get; set; }
+        int health { get; set; }
+        int skill { get; set; }
+       
+        public Clovek(string name, string surname, DateTime birth, int stamina, int health, int skill)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.birth = birth;
+            this.stamina = stamina;
+            this.health = health;
+            this.skill = skill;
+        }
+
+     
+
+    }
+}
